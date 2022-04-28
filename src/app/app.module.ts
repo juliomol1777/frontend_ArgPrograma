92b6913b82748153926f3b34dfb37f640022c9b4
julio mol <juliomolina17@gmail.com>
2022-04-28 19:43:51 -0300

    agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { NavComponent } from './componentes/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    AptitudesComponent,
+    ProyectosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
